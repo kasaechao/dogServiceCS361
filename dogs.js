22171ffb77ec0ce68aa4741965e92726055aeb44
kasaechao <you@example.com>
2021-11-08 13:38:12 -0800

    initital commit

---
+++ dogs.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const axios = require('axios').default
+app.use(express.json())
+
+
+app.get('/dog', (req, res) => {
+  res.send('hello')
+})
+
+app.post('/', (req, res) => {
+  let dog = req.body.breed
+
+  // format dog to be sent to axios to match
+  dog = dog.replace(' ', '%20').toLowerCase()
+  console.log(dog)
+
+  axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${dog}`)
+    .then((response) => {
+      console.log(response.data.query.search[0])
+      const data = response.data.query.search[0]
+      res.json(data)
+    })
+    .catch((err) => {
+      res.send('404: query incorrect, or not found')
+    })
+})
+
+app.listen(3000, () => {
+  console.log('connecting to server...')
+  }
+)

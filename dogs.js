fa5648b43dc9931a01134d21d528ef064ae1480a
kasaechao <you@example.com>
2021-11-08 16:15:09 -0800

    add cors

--- dogs.js
+++ dogs.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const axios = require('axios').default
+const cors = require('cors')
+
 app.use(express.json())
+app.use(cors())
 
 
 app.get('/', (req, res) => {
